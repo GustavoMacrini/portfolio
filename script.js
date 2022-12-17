432c47bf01ab0e809d68137486db850e5d4f3ecf
GustavoMacrini <gustavo.godencio@hotmail.com>
2022-12-17 16:50:37 -0300

    Refatoração de código js - 1 função para todos os sliders

--- script.js
+++ script.js
@@ -1,87 +1,46 @@
-// FRONT ************************
-const controlsFront = document.querySelectorAll(".control-front");
-let currentItemFront = 0;
-const itemsFront = document.querySelectorAll(".front");
-const maxitemsFront = itemsFront.length;
+let currentItem = 0;
+function mudarCertificado(type, direction){
+    let controlType;
+    let classType;
 
-controlsFront.forEach((control) => {
-    control.addEventListener("click", () => {
-        const isLeft = control.classList.contains("arrow-left");
-        
-        if(isLeft){
-            if(currentItemFront == maxitemsFront - 1 ){
-                currentItemFront -=1;
-            }else{
-                currentItemFront -=1;
-            }
+    switch(type){
+        case 'front':
+            controlType =".control-front";
+            classType = ".front";
+        break;
+        case 'back':
+            controlType = ".control-back";
+            classType = ".back";
+        break;
+        default:
+            console.log('parametro(s) errado(s)');
+    };
+
+    let controls = document.querySelectorAll(controlType);;
+    let items = document.querySelectorAll(classType);
+    let maxitems = items.length;    
+
+    if(direction == 'left'){
+        currentItem -=1;
+    }else{
+        currentItem +=1;
+    }
+
+    if(currentItem >= maxitems){
+        currentItem = 0;
+    }
+    
+    if(currentItem < 0){
+        currentItem = maxitems - 1;
+    }
             
-        }else{
-            currentItemFront +=1;
-        }
-
-        if(currentItemFront >= maxitemsFront){
-            currentItemFront = 0;
-        }
-
-        if(currentItemFront < 0){
-            currentItemFront = maxitemsFront - 1;
-        }
-
-        itemsFront.forEach(item => item.classList.remove("current-item"));
-
-        itemsFront[currentItemFront].scrollIntoView({
-            inline: "center",
-            behavior: "smooth",
-            block: "nearest"
-        });
-
-        itemsFront[currentItemFront].classList.add("current-item");
-
-        // console.log("control clicked", isLeft, currentItemFront);
-    })
-});
-
-
-// BACK******************
-const controlsBack = document.querySelectorAll(".control-back");
-let currentItemBack = 0;
-const itemsBack = document.querySelectorAll(".back");
-const maxitemsBack = itemsBack.length;
-
-controlsBack.forEach((control) => {
-    control.addEventListener("click", () => {
-        const isLeft = control.classList.contains("arrow-left");
-        
-        if(isLeft){
-            if(currentItemBack == maxitemsBack - 1 ){
-                currentItemBack -=1;
-            }else{
-                currentItemBack -=1;
-            }
-            
-        }else{
-            currentItemBack +=1;
-        }
-
-        if(currentItemBack >= maxitemsBack){
-            currentItemBack = 0;
-        }
-
-        if(currentItemBack < 0){
-            currentItemBack = maxitemsBack - 1;
-        }
-
-        itemsBack.forEach(item => item.classList.remove("current-item"));
-
-        itemsBack[currentItemBack].scrollIntoView({
-            inline: "center",
-            behavior: "smooth",
-            block: "nearest"
-        });
-
-        itemsBack[currentItemBack].classList.add("current-item");
-
-        // console.log("control clicked", isLeft, currentItemBack);
-    })
-});
-
+    items.forEach(item => item.classList.remove("current-item"));
+    
+    items[currentItem].scrollIntoView({
+        inline: "center",
+        behavior: "smooth",
+        block: "nearest"
+    });
+    
+    items[currentItem].classList.add("current-item"); 
+};
